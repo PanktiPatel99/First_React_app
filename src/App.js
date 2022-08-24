6c350573bf49fb2a27c84673d7bc755076fa44dc
PanktiPatel99 <panktip.mobio@gmail.com>
2022-08-25 00:31:56 +0530

    lifting State up (child-to-parent component communication)

--- src/App.js
+++ src/App.js
@@ -28,11 +28,16 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+
   return (
     <div className="App">
       <HelloWorld></HelloWorld>
       <h1>React Application : Expense Tracker</h1>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
